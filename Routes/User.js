fbe0b69301b996969e646695cf8e6a80eabd6b0a
Aman982816 <aman982816aman@gmail.com>
2022-10-01 15:55:32 +0000

    role added in response

--- Routes/User.js
+++ Routes/User.js
@@ -187,7 +187,7 @@ router.post('/login',
 
             const authtoken = jwt.sign(data, APPSECRET);
             success = true;
-            res.json({ success, authtoken, username: user.name })
+            res.json({ success, authtoken, username: user.name, role: user.role })
 
         } catch (error) {
             res.status(500).json({ message: error.message })
